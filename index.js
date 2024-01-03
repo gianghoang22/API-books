94279ca394c3fb88ca3f1ef032d561012050f18e
gianghoang22 <lehoanggiang220602@gmail.com>
2024-01-03 11:02:44 +0700

    Model

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+//Connect DATABASE
+mongoose.connect((process.env.MONGODB_URL), () => {
+    console.log("Connected to MongoDB");
+})
+
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(cors());
+app.use(morgan("common"));
+
+
+
+app.listen(8000, () => {
+    console.log("Server is running...");
+});
