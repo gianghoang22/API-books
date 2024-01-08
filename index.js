0a890a7915b021c5e884d7e4af73ec7148b3c0ba
Giang Hoang <lehoanggiang220602@gmail.com>
2024-01-08 16:56:34 +0700

    src

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const app = express();
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const dotenv = require("dotenv");
+const authorRoute = require("./routes/author");
+const bookRoute = require("./routes/book");
 
 dotenv.config();
-//Connect DATABASE
-mongoose.connect((process.env.MONGODB_URL), () => {
-    console.log("Connected to MongoDB");
-})
+//CONNECT DATABASE
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("Connected to MongoDB");
+});
 
-
-app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(helmet());
 app.use(cors());
 app.use(morgan("common"));
 
+//ROUTES
+app.use("/v1/author", authorRoute);
+app.use("/v1/book", bookRoute);
 
-
-app.listen(8000, () => {
-    console.log("Server is running...");
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running...");
 });
